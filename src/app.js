fd872eec80accab2e707da822e18863da3f91826
jérémy <chabert.jeremy@gmail.com>
2020-05-13 13:49:48 +0200

    Module5

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ const fs = require('fs')
 const path = require('path')
 const express = require('express')
 const app = new express()
+const accountRoutes = require('./routes/accounts.js')
+const servicesRoutes = require('./routes/services.js')
 const { users, accounts, writeJSON } = require('./data.js')
+
+app.use('/account', accountRoutes)
+app.use('/services', servicesRoutes)
 
 app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs')
@@ -15,41 +20,7 @@ app.get('/', (req, res) =>
     res.render('index', { title: 'Account Summary', accounts: accounts })
 )
 
-app.get('/savings', (req, res) =>
-    res.render('account', { account: accounts.savings })
-)
-app.get('/checking', (req, res) =>
-    res.render('account', { account: accounts.checking })
-)
-app.get('/credit', (req, res) =>
-    res.render('account', { account: accounts.credit })
-)
-
 app.get('/profile', (req, res) => res.render('profile', { user: users[0] }))
-
-app.get('/transfer', (req, res) =>
-    res.render('transfer', { account: accounts.credit })
-)
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= req.body.amount
-    accounts[req.body.to].balance += parseInt(req.body.amount)
-    writeJSON()
-    res.render('transfer', { message: 'Transfer Completed' })
-})
-
-app.get('/payment', (req, res) =>
-    res.render('payment', { account: accounts.credit })
-)
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount
-    accounts.credit.available += parseInt(req.body.amount)
-    writeJSON()
-    res.render('payment', {
-        message: 'Payment Successful',
-        account: accounts.credit,
-    })
-})
 
 app.listen(3000, () => {
     console.log('app listen 3000')
